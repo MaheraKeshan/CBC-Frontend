8ad1b17f820060e440ee87a9d5b2e626323b63d6
Mahera <maherakeshan90@gmail.com>
2025-05-01 18:06:34 +0530

    Start CBC frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/header'
+import ProductCard from './components/productCard'
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <ProductCard name="Asus Laptop" description={"lorem sdaf sad sad"} price="$1000" picture="https://th.bing.com/th/id/R.de0e84d03d4214a87f93e1446477ac08?rik=ablxJR3qRAnFDg&pid=ImgRaw&r=0"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import UserData from "./userdata";
+
+
+export default function Header() {
+	return (
+		<div className= "bg-red-500">
+			<h1 className="font-bold text-2xl text-blue-700">Crystal Beauty CLear</h1>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil amet earum nostrum alias, quas provident numquam eos officia. Illo voluptatibus obcaecati quam! Animi aperiam iure veniam illo distinctio aspernatur accusamus.</p>
+			<UserData></UserData>
+		</div>
+	);
+	// <header>
+}
